f8b9abac3270591a2ae4bc2983bad9a37783e1bb
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2026-05-26 09:32:35 +0530

    Import dashboard tab stability styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./styles/liquidControls.css";
+import "./styles/dashboardTabs.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
