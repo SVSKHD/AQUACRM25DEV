1455148ef2e99d695dce697f8d690f63c35227ce
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2026-06-14 22:20:13 +0530

    Use path-based quotation link page

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
-import QuotationPublicPage from "./pages/QuotationPublicPage";
+import QuotationLinkPage from "./pages/QuotationLinkPage";
 import "./index.css";
 import "./styles/liquidControls.css";
 import "./styles/dashboardTabs.css";
@@ -11,12 +10,6 @@ const isQuotationPage = window.location.pathname.startsWith("/quotation/");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {isQuotationPage ? (
-      <BrowserRouter>
-        <QuotationPublicPage />
-      </BrowserRouter>
-    ) : (
-      <App />
-    )}
+    {isQuotationPage ? <QuotationLinkPage /> : <App />}
   </StrictMode>,
 );
