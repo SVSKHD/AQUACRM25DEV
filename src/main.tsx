7c597fea1278e6667c6d9ed89b732b779658136b
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2026-06-14 21:42:13 +0530

    Render quotation page route

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import QuotationPage from "./pages/QuotationPage";
 import "./index.css";
 import "./styles/liquidControls.css";
 import "./styles/dashboardTabs.css";
 
+const isQuotationPage = window.location.pathname.startsWith("/quotation/");
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {isQuotationPage ? (
+      <BrowserRouter>
+        <QuotationPage />
+      </BrowserRouter>
+    ) : (
+      <App />
+    )}
   </StrictMode>,
 );
