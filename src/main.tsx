149b48171d1ed541e3ee0c3a3a51a48ddee8a2ff
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2026-06-15 07:49:42 +0530

    Load compact invoice dialog mobile styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import QuotationLinkPage from "./pages/QuotationLinkPage";
 import "./index.css";
 import "./styles/liquidControls.css";
 import "./styles/dashboardTabs.css";
+import "./styles/invoiceDialogMobile.css";
 
 const isQuotationPage = window.location.pathname.startsWith("/quotation/");
 
